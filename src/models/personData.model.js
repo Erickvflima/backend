6b70b125bd1cf6242ca752a750d20ae7917b4b10
erick.lima <erick.lima@vileve.com.br>
2022-05-23 16:04:52 -0300

    finished

--- src/models/personData.model.js
+++ src/models/personData.model.js
@@ -17,6 +17,14 @@ function postList(body) {
   try {
     let data = JSON.stringify(body, null, 2);
 
+    // let currentData = `${JSON.stringify(
+    //   personDataJson,
+    //   null,
+    //   2
+    // )},${JSON.stringify(body, null, 2)}`;
+    // let currentData = [...personDataJson, body];
+    // console.log(currentData);
+
     fs.writeFile("personData.json", data, (err) => {
       if (err) throw err;
       console.log("Data written to file");
