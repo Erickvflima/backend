f308b90d357beab8efa23298ef14562375f2f705
erick.lima <erick.lima@vileve.com.br>
2022-05-23 00:03:08 -0300

    api get

--- src/models/personData.model.js
+++ src/models/personData.model.js
@@ -16,6 +16,24 @@ function getList() {
           nascimento: "04/02/1999",
           telefone: "(31)98467-1212",
         },
+        {
+          name: "Rosely Lima",
+          email: "Rosely@gmail.com",
+          nascimento: "04/02/1988",
+          telefone: "(31)98467-3333",
+        },
+        {
+          name: "Willian Lima",
+          email: "Willian@gmail.com",
+          nascimento: "04/02/2000",
+          telefone: "(31)98467-1111",
+        },
+        {
+          name: "Carlos Santos",
+          email: "Carlos@gmail.com",
+          nascimento: "04/02/2003",
+          telefone: "(31)98467-9999",
+        },
       ],
     };
     return response;
